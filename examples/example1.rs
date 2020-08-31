2fe234d5699c6d5764a1511e14dace7219bed0b6
monaqa <mogassy@yahoo.co.jp>
2020-09-01 00:37:30 +0900

    improve output of example

--- examples/example1.rs
+++ examples/example1.rs
@@ -1,4 +1,5 @@
 use pest::Parser;
+use pest::iterators::{Pair, Pairs};
 use satysfi_peg_parser::{SatysfiParser, Rule};
 
 use std::fs;
@@ -6,8 +7,27 @@ use std::fs;
 fn main() {
 
     let unparsed_file = fs::read_to_string("examples/example1.saty").expect("cannot read file");
-    let pairs = SatysfiParser::parse(Rule::program, &unparsed_file);
+    let mut pairs = SatysfiParser::parse(Rule::program, &unparsed_file).unwrap();
 
-    dbg!(&pairs);
+    // let header = pairs.next().unwrap();
+    // dbg!(&header.into_inner().next().unwrap().into_inner().next());
 
+    // expand_pairs(&mut pairs, 0)
+    for p in pairs {
+        expand_pairs(p, 0)
+    }
+
+}
+
+fn expand_pairs(pair: Pair<Rule>, indent: usize) {
+    let space = "  ".repeat(indent);
+    let pairs = pair.clone().into_inner();
+    if pairs.clone().count() == 0 {
+        println!(r#"{}{:?}: "{}""#, space, pair.as_rule(), pair.as_str());
+    } else {
+        println!("{}[{:?}]", space, pair.as_rule());
+    }
+    for p in pairs {
+        expand_pairs(p, indent + 1);
+    }
 }
